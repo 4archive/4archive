d121d285f978893fedbccaed91430010ed79c8c4
4archive <admin@4archive.org>
2015-05-09 14:00:29 -0400

    disabled archiving by default

--- config/archive.php
+++ config/archive.php
@@ -9,5 +9,6 @@
 return [
 	'nsfw' => ['b', 'd', 'e', 'f', 'gif', 'h', 'hr',
 		'r', 's', 't', 'u', 'wg', 'i', 'ic', 'r9k',
-		's4s', 'hm', 'y', 'hc', 'pol', 'soc']
+		's4s', 'hm', 'y', 'hc', 'pol', 'soc'],
+	'disabled' => true
 ];
