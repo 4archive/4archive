e6bb075104b26ccd9d836953a45a9851f50d39a4
4archive <admin@4archive.org>
2015-05-13 12:06:50 -0400

    404 err0rs

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 use Exception;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException as NotFoundHttpException;
 
 class Handler extends ExceptionHandler {
 
@@ -36,6 +37,10 @@ class Handler extends ExceptionHandler {
      */
     public function render($request, Exception $e)
     {
+        if ($e instanceof NotFoundHttpException) {
+            return view('errors.404');
+        }
+
         return parent::render($request, $e);
     }
 
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('master')
+
+@section('title', '404 - 4archive')
+@section('contentHeader', '404')
+
+@section('content')
+<div class="hp-content">
+	Ha! You went to a page that doesn't exist.
+</div>
+@stop
